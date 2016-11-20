31c4cf2d3c108fa9ca50bd418f5f3a0a6dd83f08
Todd Isaacs <teisaacs21w@gmail.com>
2016-11-20 10:29:44 -0500

    Initial Commit

---
+++ TICircleProgress/TICircleProgress.h
@@ -0,0 +1,19 @@
+//
+//  TICircleProgress.h
+//  TICircleProgress
+//
+//  Created by Todd Isaacs on 11/20/16.
+//  Copyright © 2016 Todd Isaacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TICircleProgress.
+FOUNDATION_EXPORT double TICircleProgressVersionNumber;
+
+//! Project version string for TICircleProgress.
+FOUNDATION_EXPORT const unsigned char TICircleProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TICircleProgress/PublicHeader.h>
+
+
